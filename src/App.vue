44d7c97230b3d65b90cc34317d676d544607e0b6
Azizbek Kurbanov <azizbekzaripov2003@gmail.com>
2024-09-04 06:56:11 +0500

    Button and dropdown

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { ref } from 'vue'
-import { normalizePageHash } from './functions'
+import { normalizePageHash, generateTimelineItems } from './functions'
 import TheHeader from './components/TheHeader.vue'
 import TheNav from './components/TheNav.vue'
 import TheTimeline from './pages/TheTimeline.vue'
@@ -10,6 +10,7 @@ import TheProgress from './pages/TheProgress.vue'
 import { PAGE_TIMELINE, PAGE_ACTIVITIES, PAGE_PROGRESS } from './constants'
 
 const currentPage = ref(normalizePageHash())
+const timelineItems = generateTimelineItems()
 
 function goTo(page){
   currentPage.value = page
@@ -23,7 +24,7 @@ function goTo(page){
   @go-to-progress="goTo(PAGE_PROGRESS)" />
 
   <main class="flex flex-grow flex-col">
-    <TheTimeline v-show="currentPage === PAGE_TIMELINE" />
+    <TheTimeline v-show="currentPage === PAGE_TIMELINE" :timeline-items="timelineItems"/>
     <TheActivities v-show="currentPage === PAGE_ACTIVITIES" />
     <TheProgress v-show="currentPage === PAGE_PROGRESS" />
   </main>
